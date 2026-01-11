1b6e38b2e8b1228868f9a3366f491903e13c3350
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2026-01-11 12:10:19 -0500

    signup test passing

--- server/app.py
+++ server/app.py
@@ -6,6 +6,14 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, UserSchema
 
+class Signup(Resource):
+    def post(self):
+        json=request.get_json()
+        user=User(username=json['username'])
+        db.session.add(user)
+        db.session.commit()
+        return UserSchema().dump(user),201
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -16,6 +24,7 @@ class ClearSession(Resource):
         return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
