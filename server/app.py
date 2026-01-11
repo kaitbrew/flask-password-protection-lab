8bb7dafc11b07991d27b909c1ac8921099ab3035
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2026-01-11 12:14:26 -0500

    login test passing

--- server/app.py
+++ server/app.py
@@ -22,9 +22,21 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+    
+class Login(Resource):
+    def post(self):
+        username=request.get_json()['username']
+        password=request.get_json()['password']
+        user=User.query.filter(User.username==username).first()
+
+        if user and password:
+            session['user_id']=user.id
+            return UserSchema().dump(user),200
+        return {'error':'401 Unauthorized'},401
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup')
+api.add_resource(Login,'/login')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
